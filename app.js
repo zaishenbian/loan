3eb50e8330eda6f0bd243caf0eb2a3d48ba34f0e
cherish-your-smile <779458847@qq.com>
2018-08-06 00:02:48 +0800

    去掉请求头设置

--- app.js
+++ app.js
@@ -44,15 +44,6 @@ mongoose.connect('mongodb://localhost:27017/loan', function(err){
   }
 });
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
-
 app.use('/ssm', users);
 app.use('/api',api);
 app.use('/adminApi',admin);
