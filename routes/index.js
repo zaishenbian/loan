3151dab54b1ec5afb269d0cd3f9fea95b8a0c918
cherish-your-smile <779458847@qq.com>
2018-08-05 15:18:33 +0800

    添加openId

--- routes/index.js
+++ routes/index.js
@@ -56,15 +56,14 @@ router.get('/:first?/:second?', function(req, res, next) {
   var second = req.params.second?'/'+req.params.second:"";
   var redirect = first+second;
   var openId = req.query.openId;
-  // if(openId){
-  //   res.render(first+second);
-  // }else{
-  //   /* 获取openId */
-  //   var redirect_uri = encodeURI(pageUrl+'/api/getopenId?page='+first+second);
-  //   var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appId+'&redirect_uri='+redirect_uri+'&response_type=code&scope=snsapi_base#wechat_redirect';
-  //   res.redirect(url);
-  // }
-  res.render(first+second);
+  if(openId){
+    res.render(first+second);
+  }else{
+    /* 获取openId */
+    var redirect_uri = encodeURI(pageUrl+'/api/getopenId?page='+first+second);
+    var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appId+'&redirect_uri='+redirect_uri+'&response_type=code&scope=snsapi_base#wechat_redirect';
+    res.redirect(url);
+  }
 });
 
 module.exports = router;
